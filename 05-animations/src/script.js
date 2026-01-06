3c503067908fbf86344a9c9c3fbf6c08fab608d9
Pablo Calle <callejupa@gmail.com>
2026-01-06 18:43:55 -0500

    animation in threejs and gsap basic animation

---
+++ 05-animations/src/script.js
@@ -0,0 +1,62 @@
+import * as THREE from 'three'
+import gsap from 'gsap'
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0x0000ff })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+// Time in pure javascript
+// let time = Date.now()
+
+// Clock
+const clock = new THREE.Clock()
+
+// Animate with gsap
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2})
+gsap.to(mesh.position, { duration: 1, delay: 2, x: 0})
+
+// Animations
+const tick = () => {
+    // Time in pure javascript
+    // const currentTime = Date.now()
+    // const deltaTime = currentTime - time
+    // time = currentTime
+
+    // Clock
+    // const elapsedTime = clock.getElapsedTime()
+    // Update objects
+
+    // mesh.rotation.y = elapsedTime
+    // mesh.position.y = Math.sin(elapsedTime)
+    // mesh.position.x = Math.cos(elapsedTime)
+    // mesh.position.z = Math.sin(elapsedTime)
+    // Render
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+}
+
+tick()
