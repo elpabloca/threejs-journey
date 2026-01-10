876686338f040ebb3add5ab9a9b8eee7bd4eb6d6
Pablo Calle <callejupa@gmail.com>
2026-01-09 20:20:18 -0500

    add orbit controls

--- 06-cameras/src/script.js
+++ 06-cameras/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
 /**
  * Cursor
@@ -11,7 +12,6 @@ const cursor = {
 window.addEventListener('mousemove', (event) => {
     cursor.x = event.clientX / sizes.width - 0.5
     cursor.y = - (event.clientY / sizes.height - 0.5)
-    console.log(cursor.x)
 })
 
 /**
@@ -48,6 +48,10 @@ camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
@@ -64,11 +68,14 @@ const tick = () =>
     // // Update objects
     // mesh.rotation.y = elapsedTime;
 
-    // Update Camera
-    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-    camera.position.y = cursor.y * 10
-    camera.lookAt(mesh.position)
+    // // Update Camera
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    // camera.position.y = cursor.y * 10
+    // camera.lookAt(mesh.position)
+
+    // Update controls
+    controls.update()
     // Render
     renderer.render(scene, camera)
 
